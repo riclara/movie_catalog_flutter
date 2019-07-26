52fc40f5f08de5182cb97bb697034e9dbdc17bed
Ricardo Lara <riclara@gmail.com>
2019-07-25 22:11:14 -0500

    add movie model

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:movie_catalog_flutter/src/widgets/card_swiper_widget.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -27,20 +27,9 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _swiperCards() {
-    return Container(
-      padding: EdgeInsets.only(top: 10.0),
-      width: double.infinity,
-      height: 300.0,
-      child: Swiper(
-        itemBuilder: (BuildContext context, int index){
-          return new Image.network("http://via.placeholder.com/350x150",fit: BoxFit.fill,);
-        },
-        itemCount: 3,
-        itemWidth: 200.0,
-        pagination: new SwiperPagination(),
-        control: new SwiperControl(),
-        layout: SwiperLayout.STACK,
-      ),
+
+    return  CardSwiper(
+      elements: [1,2,3,4,5],
     );
   }
 }
